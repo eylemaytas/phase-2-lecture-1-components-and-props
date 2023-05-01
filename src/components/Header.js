456d772497136f64623f05c49c0fefb3ca9101ff
Eylem Aytas <eylem.aytas@hotmail.com>
2023-05-01 15:24:42 -0400

    Modifies and adds some files

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+        <header>
+        <h1>
+            Flatapets
+            <span className="logo" role="img">
+            🐈
+            </span>
+        </h1>
+        </header>
+    );
+}
+export default Header;
